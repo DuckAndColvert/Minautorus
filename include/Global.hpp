a2f37d4b90b3bd8161414236835b017528aa6816
Bog13 <berenger.ossete_gombe@edu.univ-fcomte.fr>
2015-03-16 20:55:23 +0100

    VertexArray used for displaying the map (just a black screen for now)

--- include/Global.hpp
+++ include/Global.hpp
@@ -4,5 +4,16 @@
 #define DATA_PATH "data/"
 #define IMG_PATH "img/"
 
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+#define WIDTH 640
+#define HEIGHT 480
+
+#define BLOC_WIDTH 16
+#define BLOC_HEIGHT BLOC_WIDTH
+
+#define NB_TILE_WIDTH ((WIDTH)/(BLOC_WIDTH))
+#define NB_TILE_HEIGHT ((HEIGHT)/(BLOC_HEIGHT))
 
 #endif
--- include/InGame.hpp
+++ include/InGame.hpp
@@ -3,7 +3,23 @@
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <Global.hpp>
 #include <Scene.hpp>
+
+enum TileType
+  {
+    GROUND = 0,
+    WALL = 0
+  };
+
+struct Tile
+{
+  size_t i;
+  size_t j;
+
+  TileType type;
+  bool obstacle;
+};
 
 class InGame: public Scene
 {
@@ -19,7 +35,11 @@ private:
   InGame(InGame const& ig) = delete;
 
   
+  Tile* m_tiles[NB_TILE_HEIGHT][NB_TILE_WIDTH];
+  void initTiles();
 
+  sf::VertexArray m_vertex_array;
+  void createVertexTile(size_t i, size_t j,size_t w,size_t h, sf::Color color);
   
 };
 
